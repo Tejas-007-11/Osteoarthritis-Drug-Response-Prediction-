2e68ebd79cb8d9d3f6c0ba740b16b4ec25c60018
Tejas krishna <tejaskrishna.as@gmail.com>
2025-05-17 13:01:23 +0530

    Frontend

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 import joblib
 import numpy as np
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Allow requests from frontend
+CORS(app) 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 # Load the trained model and encoders
 model = joblib.load('oa_drug_response_model.pkl')
